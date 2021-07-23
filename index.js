af71d6c79c75526837fc68cc80708a8722d759ae
unlimited-element <73407996+unlimited-element@users.noreply.github.com>
2021-07-22 23:01:10 -0700

    Added a fade in for the nucleus
    Created fade in for nucleus, removed spinning wheel. Site may look better with simple progress bar

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const swap = document.getElementsByTagName('body');
+console.log(swap);
+// const swapper = () => {
+//     const toggler = () => document.querySelector('nucleus').style.display = 'block';
+//     return setTimeout(toggler, 3000);
+// }
+
+// swap.addEventListener('load', swapper);
