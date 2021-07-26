fe9d57b1d8be89ffb74ad8eb95fa314e9d98fe32
unlimited-element <73407996+unlimited-element@users.noreply.github.com>
2021-07-25 23:34:30 -0700

    Added a footer and extended dark mode
    Added footer and added dark mode to both navbar and footer

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const swap = document.getElementById('btn-swapper');
+
+const swapper = () => {
+    return document.body.classList.toggle('dark-theme')
+}
+
+let checkSwap = swap.onclick = swapper;
+
+console.log(checkSwap);
