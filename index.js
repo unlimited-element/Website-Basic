e44ad8ba3f5d416f0e77b850b167e6e0c84737f6
unlimited-element <73407996+unlimited-element@users.noreply.github.com>
2021-07-26 23:44:41 -0700

    Fixed navbar toggler and added dark/light mode button toggler
    There was an issue where the navbar toggler didn't show up in mobile viewport

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 'use strict'
 
-const swap = document.getElementById('btn-swapper');
+var swap = document.getElementById('btn-swapper');
 
 const swapper = () => {
-    return document.body.classList.toggle('dark-theme')
+
+    var nav = document.querySelector("nav");
+    var classes = nav.classList;
+    if (classes[3] == "navbar-light") {
+        document.body.classList.toggle("dark-theme")
+        classes.replace("navbar-light", "navbar-dark")
+        document.getElementById('btn-swapper').innerText = "Light Theme";
+        } else if (classes[3] == "navbar-dark") {
+        document.body.classList.toggle("dark-theme");
+        classes.replace("navbar-dark", "navbar-light");
+        document.getElementById('btn-swapper').innerText = "Dark Theme";
+    }
+
 }
+const checkSwap = swap.onclick = swapper;
+// ----------------------------------
 
-let checkSwap = swap.onclick = swapper;
-
-console.log(checkSwap);
+// const updateInnerText = () => {
+//     if (document.body.classList[0] == "dark-theme") {
+//         document.getElementById('btn-swapper').innerText = "Light Theme";
+//     }
+// }
+// const buttonSwap = document.getElementsByClassName("btn")[0].innerText.onclick = updateInnerText;
